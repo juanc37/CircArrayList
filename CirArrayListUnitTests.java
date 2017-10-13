68e3ef44ca2f6c98a5ca70338deee627a65d7702
Juan <juan@dreamvent.com>
2017-10-13 13:19:43 -0700

    added setup for some unit tests

---
+++ CirArrayListUnitTests.java
@@ -0,0 +1,68 @@
+package CirArray;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Unit tests modified in class ...
+ *
+ */
+public class CirArrayListTests extends TestCase {
+
+    private static final int testSize = 2048;
+    private static final int valueInvalid = -1;
+    private static final int valueValid = 310;
+
+    private List<Integer> sut;
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CirArrayListTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(CirArrayListTest.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        sut = new CirArrayList<>();
+    }
+
+    public void test_constructorDefault_initializedCorrectly() {
+        assertThat(sut.size(), is(equalTo(0)));
+    }
+
+    public void test_constructorCollection_initializedCorrectly() {
+
+        final Integer[] values = getSequentialIntArray(testSize);
+        sut = new CirArrayList<>(Arrays.asList(values));
+
+        assertThat(sut.size(), is(equalTo(values.length)));
+        assertTrue(sut.containsAll(Arrays.asList(values)));
+    }
+
+    private Integer[] getSequentialIntArray(int size) {
+        Integer[] values = new Integer[size];
+        for (int i = 0; i < size; i++)
+            values[i] = i;
+
+        return values;
+    }
+}
